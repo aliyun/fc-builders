5d59517bf3b53c29b456122ee0f2089fb03ab582
Hehe Tan <xiayule148@gmail.com>
2019-08-25 20:44:10 +0800

    add binary and upload support (#2)

---
+++ binary.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+PKG_VERSION=`node -p "require('./package').version"`
+
+## package to binary
+pkg -t node8-linux-x64 --out-path -o ./output/fun-install-linux-64 .
+
+## rename and zip output files
+cd output
+
+for f in fun-install-*; 
+do 
+	filename=fun-install-v${PKG_VERSION}${f##*fun-install}
+	mv $f $filename
+	zip $filename.zip $filename
+	rm $filename
+done
