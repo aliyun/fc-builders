c3438a422bd3ffba27361bf49ffffe8e6c55572d
Hehe Tan <xiayule148@gmail.com>
2019-08-29 14:10:28 +0800

    add target parameter support for fun-install (#4)

--- bin/fun-install-apt-get.js
+++ bin/fun-install-apt-get.js
@@ -1,13 +1,16 @@
+'use strict';
 
 const program = require('commander');
 
 const handler = require('../lib/exception-handler');
 
-program.command('install [packages...]')
+program.command('install')
+  .usage('[options] [packages...]')
   .description('Install apt packages')
-  .arguments('packages')
+  .option('-t, --target <target>', 'path to install')
+  .arguments('[packages...]')
   .action((packages) => {
-    require('../lib/commands/apt-get')(packages, program).catch(handler);
+    require('../lib/commands/apt-get')(packages, program.commands[0]).catch(handler);
   });
 
 program.parse(process.argv);
--- bin/fun-install-pip.js
+++ bin/fun-install-pip.js
@@ -1,13 +1,16 @@
+'use strict';
 
 const program = require('commander');
 
 const handler = require('../lib/exception-handler');
 
-program.command('install [packages...]')
+program.command('install')
+  .usage('-t|--target <target> [packages...]')
   .description('Install pip packages')
-  .arguments('packages')
+  .option('-t, --target <target>', 'path to install')
+  .arguments('[packages...]')
   .action((packages) => {
-    require('../lib/commands/pip')(packages, program).catch(handler);
+    require('../lib/commands/pip')(packages, program.commands[0]).catch(handler);
   });
 
 program.parse(process.argv);
