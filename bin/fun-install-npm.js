99d5e79daf4871634aaa4713cd71ca24dada2fd9
coco-super <sunfeiyu_java@163.com>
2020-04-09 17:31:11 +0800

    ellison/support install private registry dependencies

--- bin/fun-install-npm.js
+++ bin/fun-install-npm.js
@@ -8,6 +8,7 @@ program.command('install')
   .usage('-t|--target <target> [packages...]')
   .description('Install npm packages')
   .option('-t, --target <target>', 'path to install')
+  .option('--registry <url>', 'Configure npm to use any compatible registry, and even run your own registry.')
   .arguments('[packages...]')
   .action((packages) => {
     require('../lib/commands/npm')(packages, program.commands[0]).catch(handler);
--- bin/fun-install-pip.js
+++ bin/fun-install-pip.js
@@ -8,6 +8,8 @@ program.command('install')
   .usage('-t|--target <target> [packages...]')
   .description('Install pip packages')
   .option('-t, --target <target>', 'path to install')
+  .option('-i, --index-url <url>', `Base URL of Python Package Index (default https://pypi.org/simple). This should point to a repository compliant with PEP 503 (the simple repository API) or a local
+                       directory laid out in the same format.\n`)
   .arguments('[packages...]')
   .action((packages) => {
     require('../lib/commands/pip')(packages, program.commands[0]).catch(handler);
