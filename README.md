300a8ce1d2b3adb996e26cb22f0684707d9c66ea
rsonghuster <ls_huster@163.com>
2022-04-14 10:16:33 +0800

    refine builder

--- README.md
+++ README.md
@@ -2,12 +2,18 @@
 ## 环境变量说明
 
 1. BUILD_EXCLIUDE_FILES
+```
   示例: BUILD_EXCLIUDE_FILES=.s;s.yaml;
   描述: 自定义 build 时需要排除的文件
+```
 2. PHP_INI_SCAN_DIR
 3. TOOL_CACHE_PATH
+```
   示例: TOOL_CACHE_PATH=.s
   描述: 工具存放的缓存目录
+```
 4. ONLY_CPOY_MANIFEST_FILE
+```
   示例: ONLY_CPOY_MANIFEST_FILE=true
   描述: copy 代码目录时仅 copy，依赖清单文件
+```
--- bin/fun-install.js
+++ bin/fun-install.js
@@ -6,13 +6,21 @@
 
 const program = require('commander');
 
-program
-  .name('fun local')
-  .description('build function codes or install related depedencies for Function Compute')
-  .command('apt-get', 'install apt packages').alias('apt')
-  .command('pip', 'install pip packages')
-  .command('npm', 'install npm packages')
-  .command('build', 'build function codes for Function Compute (Only used for funcraft)');
+if(process.env.TOOL_CACHE_PATH === '.s') {
+  program
+    .name('s sandbox')
+    .description('build function codes or install related depedencies for Function Compute')
+    .command('apt-get', 'install apt packages').alias('apt')
+    .command('pip', 'install pip packages')
+} else {
+  program
+    .name('fun local')
+    .description('build function codes or install related depedencies for Function Compute')
+    .command('apt-get', 'install apt packages').alias('apt')
+    .command('pip', 'install pip packages')
+    .command('npm', 'install npm packages')
+    .command('build', 'build function codes for Function Compute (Only used for funcraft)');
+}
 
 require('../lib/utils/command').registerCommandChecker(program);
 
