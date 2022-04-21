99c0deb552b3d263848ce2292e434628c9df4608
wss-git <wssgryx@163.com>
2022-04-21 15:00:55 +0800

    support install args

--- bin/fun-install.js
+++ bin/fun-install.js
@@ -6,12 +6,13 @@
 
 const program = require('commander');
 
-if(process.env.TOOL_CACHE_PATH === '.s') {
+if (process.env.S_SANDBOX === 's_sandbox') {
+  process.env.TOOL_CACHE_PATH = '.s'; // 将依赖下载的到指定目录
   program
     .name('s sandbox')
     .description('build function codes or install related depedencies for Function Compute')
     .command('apt-get', 'install apt packages').alias('apt')
-    .command('pip', 'install pip packages')
+    .command('pip', 'install pip packages');
 } else {
   program
     .name('fun local')
